2322674bba7b57680e74d35a24716e1fddf97731
nvsufood <149187822+nvsufood@users.noreply.github.com>
2023-10-28 10:42:52 +0800

    Add files via upload

---
+++ SQL/online_rest.sql
@@ -0,0 +1,336 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 05, 2023 at 04:42 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `online_rest`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `adm_id` int(222) NOT NULL,
+  `username` varchar(222) NOT NULL,
+  `password` varchar(222) NOT NULL,
+  `email` varchar(222) NOT NULL,
+  `code` varchar(222) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`adm_id`, `username`, `password`, `email`, `code`, `date`) VALUES
+(6, 'admin', '81dc9bdb52d04dc20036dbd8313ed055', 'admin@gmail.com', '', '2018-04-09 07:36:18');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_codes`
+--
+
+CREATE TABLE `admin_codes` (
+  `id` int(222) NOT NULL,
+  `codes` varchar(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `admin_codes`
+--
+
+INSERT INTO `admin_codes` (`id`, `codes`) VALUES
+(1, 'QX5ZMN'),
+(2, 'QFE6ZM'),
+(3, 'QMZR92'),
+(4, 'QPGIOV'),
+(5, 'QSTE52'),
+(6, 'QMTZ2J');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dishes`
+--
+
+CREATE TABLE `dishes` (
+  `d_id` int(222) NOT NULL,
+  `rs_id` int(222) NOT NULL,
+  `title` varchar(222) NOT NULL,
+  `slogan` varchar(222) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `img` varchar(222) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `dishes`
+--
+
+INSERT INTO `dishes` (`d_id`, `rs_id`, `title`, `slogan`, `price`, `img`) VALUES
+(16, 52, 'Houlihans Mini Cheeseburger', 'Creekstone Farms, where no antibiotics or growth hormones are used', 22.55, '5ad75a5dbb329.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `remark`
+--
+
+CREATE TABLE `remark` (
+  `id` int(11) NOT NULL,
+  `frm_id` int(11) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `remark` mediumtext NOT NULL,
+  `remarkDate` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `remark`
+--
+
+INSERT INTO `remark` (`id`, `frm_id`, `status`, `remark`, `remarkDate`) VALUES
+(62, 32, 'in process', 'hi', '2018-04-18 17:35:52'),
+(63, 32, 'closed', 'cc', '2018-04-18 17:36:46'),
+(64, 32, 'in process', 'fff', '2018-04-18 18:01:37'),
+(65, 32, 'closed', 'its delv', '2018-04-18 18:08:55'),
+(66, 34, 'in process', 'on a way', '2018-04-18 18:56:32'),
+(67, 35, 'closed', 'ok', '2018-04-18 18:59:08'),
+(68, 37, 'in process', 'on the way!', '2018-04-18 19:50:06'),
+(69, 37, 'rejected', 'if admin cancel for any reason this box is for remark only for buter perposes', '2018-04-18 19:51:19'),
+(70, 37, 'closed', 'delivered success', '2018-04-18 19:51:50'),
+(71, 40, 'in process', 'sdsdsd', '2023-10-04 14:50:35');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `restaurant`
+--
+
+CREATE TABLE `restaurant` (
+  `rs_id` int(222) NOT NULL,
+  `c_id` int(222) NOT NULL,
+  `title` varchar(222) NOT NULL,
+  `email` varchar(222) NOT NULL,
+  `phone` varchar(222) NOT NULL,
+  `url` varchar(222) NOT NULL,
+  `o_hr` varchar(222) NOT NULL,
+  `c_hr` varchar(222) NOT NULL,
+  `o_days` varchar(222) NOT NULL,
+  `address` text NOT NULL,
+  `image` text NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `restaurant`
+--
+
+INSERT INTO `restaurant` (`rs_id`, `c_id`, `title`, `email`, `phone`, `url`, `o_hr`, `c_hr`, `o_days`, `address`, `image`, `date`) VALUES
+(52, 9, 'NVSU', 'nvsu@gmail.com', '09876543211', 'nvsu.com', '6am', '8pm', '24hr-x7', 'Zone 1 Barangay Tibay', '651d6b6de6bcf.jpg', '2023-10-04 13:41:01');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `res_category`
+--
+
+CREATE TABLE `res_category` (
+  `c_id` int(222) NOT NULL,
+  `c_name` varchar(222) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `res_category`
+--
+
+INSERT INTO `res_category` (`c_id`, `c_name`, `date`) VALUES
+(9, 'All', '2023-10-04 13:13:26');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `u_id` int(222) NOT NULL,
+  `username` varchar(222) NOT NULL,
+  `f_name` varchar(222) NOT NULL,
+  `l_name` varchar(222) NOT NULL,
+  `email` varchar(222) NOT NULL,
+  `phone` varchar(222) NOT NULL,
+  `password` varchar(222) NOT NULL,
+  `address` text NOT NULL,
+  `status` int(222) NOT NULL DEFAULT 1,
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`u_id`, `username`, `f_name`, `l_name`, `email`, `phone`, `password`, `address`, `status`, `date`) VALUES
+(33, 'mkdirlove', 'mkdirlove', 'mkdirlove', 'mkdirlove@lspu.edu.ph', '09876543211', '266b327d8120c75c67bd347eabf7ab49', 'mkdirlove', 1, '2023-10-04 12:55:01'),
+(34, 'Ange', 'Angelo', 'Ibarra', 'angeloibarra71@gmail.com', '09123456789', '25d55ad283aa400af464c76d713c07ad', 'Bayombong', 1, '2023-10-04 14:10:16'),
+(35, 'Mela', 'Melrose', 'Acedo', 'melroseacedo@gmail.com', '+1639100654797', 'ae245176d5df59e6ba8bbe267d0c1490', 'Sitio di mabirukam barangay hanapan, Nueva Vizcaya', 1, '2023-10-04 14:17:48'),
+(36, 'Badong', 'angelo', 'ibarra', 'wang8perez@gmail.com', '09562035409', '6eea9b7ef19179a06954edd0f6c05ceb', 'bayombong ', 1, '2023-10-04 14:28:15'),
+(37, '', 'Analyn', 'Del Rosario', 'analyncatimbang18@gmail.com', '09169112902', 'be83a8f87ff40208612d1dcd617268f7', 'Asdfgh', 1, '2023-10-04 14:39:57');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_orders`
+--
+
+CREATE TABLE `users_orders` (
+  `o_id` int(222) NOT NULL,
+  `u_id` int(222) NOT NULL,
+  `title` varchar(222) NOT NULL,
+  `quantity` int(222) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `status` varchar(222) DEFAULT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `users_orders`
+--
+
+INSERT INTO `users_orders` (`o_id`, `u_id`, `title`, `quantity`, `price`, `status`, `date`) VALUES
+(37, 31, 'jklmno', 5, 17.99, 'closed', '2018-04-18 19:51:50'),
+(38, 31, 'Red Robins Chick on a Stick', 2, 34.99, NULL, '2018-04-18 19:52:34'),
+(39, 33, 'Houlihans Mini Cheeseburger', 2, 22.55, NULL, '2023-10-04 13:35:53'),
+(40, 34, 'Houlihans Mini Cheeseburger', 1, 22.55, 'in process', '2023-10-04 14:50:35'),
+(41, 34, 'Houlihans Mini Cheeseburger', 1, 22.55, NULL, '2023-10-04 14:11:23'),
+(42, 35, 'Houlihans Mini Cheeseburger', 1, 22.55, NULL, '2023-10-04 14:19:28'),
+(43, 36, 'Houlihans Mini Cheeseburger', 1, 22.55, NULL, '2023-10-04 14:29:12'),
+(44, 36, 'Houlihans Mini Cheeseburger', 1, 22.55, NULL, '2023-10-04 14:29:35'),
+(45, 36, 'Houlihans Mini Cheeseburger', 4, 22.55, NULL, '2023-10-04 14:43:07');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`adm_id`);
+
+--
+-- Indexes for table `admin_codes`
+--
+ALTER TABLE `admin_codes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `dishes`
+--
+ALTER TABLE `dishes`
+  ADD PRIMARY KEY (`d_id`);
+
+--
+-- Indexes for table `remark`
+--
+ALTER TABLE `remark`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `restaurant`
+--
+ALTER TABLE `restaurant`
+  ADD PRIMARY KEY (`rs_id`);
+
+--
+-- Indexes for table `res_category`
+--
+ALTER TABLE `res_category`
+  ADD PRIMARY KEY (`c_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`u_id`);
+
+--
+-- Indexes for table `users_orders`
+--
+ALTER TABLE `users_orders`
+  ADD PRIMARY KEY (`o_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `adm_id` int(222) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `admin_codes`
+--
+ALTER TABLE `admin_codes`
+  MODIFY `id` int(222) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `dishes`
+--
+ALTER TABLE `dishes`
+  MODIFY `d_id` int(222) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `remark`
+--
+ALTER TABLE `remark`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=72;
+
+--
+-- AUTO_INCREMENT for table `restaurant`
+--
+ALTER TABLE `restaurant`
+  MODIFY `rs_id` int(222) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
+
+--
+-- AUTO_INCREMENT for table `res_category`
+--
+ALTER TABLE `res_category`
+  MODIFY `c_id` int(222) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `u_id` int(222) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+
+--
+-- AUTO_INCREMENT for table `users_orders`
+--
+ALTER TABLE `users_orders`
+  MODIFY `o_id` int(222) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ test.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+include("connection/connect.php"); //INCLUDE CONNECTION
+error_reporting(0); // hide undefine index errors
+session_start(); // temp sessions
+if(isset($_POST['submit']))   // if button is submit
+{
+	$username = $_POST['username'];  //fetch records from login form
+	$password = $_POST['password'];
+	
+	if(!empty($_POST["submit"]))   // if records were not empty
+     {
+	$loginquery ="SELECT * FROM users WHERE username='$username' && password='".md5($password)."'"; //selecting matching records
+	$result=mysqli_query($db, $loginquery); //executing
+	$row=mysqli_fetch_array($result);
+	
+	                        if(is_array($row))  // if matching records in the array & if everything is right
+								{
+                                    	$_SESSION["user_id"] = $row['u_id']; // put user id into temp session
+										 header("refresh:1;url=index.php"); // redirect to index.php page
+	                            } 
+							else
+							    {
+                                      	$message = "Invalid Username or Password!"; // throw error
+                                }
+	 }
+	
+	
+}
+?>
+
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="#">
+    <title>NVSU Ordering System</title>
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/animsition.min.css" rel="stylesheet">
+    <link href="css/animate.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet"> </head>
+<body>
+     
+         <!--header starts-->
+         <header id="header" class="header-scroll top-header headrom">
+            <!-- .navbar -->
+            <nav class="navbar navbar-dark">
+               <div class="container">
+                  <button class="navbar-toggler hidden-lg-up" type="button" data-toggle="collapse" data-target="#mainNavbarCollapse">&#9776;</button>
+                  <a class="navbar-brand" href="index.php"> <img class="img-rounded" src="images/logo.png" alt="" height="30"> NVSU Ordering System</a>
+                  <div class="collapse navbar-toggleable-md  float-lg-right" id="mainNavbarCollapse">
+                     <ul class="nav navbar-nav">
+							<li class="nav-item"> <a class="nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a> </li>
+                            <li class="nav-item"> <a class="nav-link active" href="restaurants.php">Restaurants <span class="sr-only"></span></a> </li>
+                            
+							<?php
+						if(empty($_SESSION["user_id"]))
+							{
+								echo '<li class="nav-item"><a href="login.php" class="nav-link active">login</a> </li>
+							  <li class="nav-item"><a href="registration.php" class="nav-link active">signup</a> </li>';
+							}
+						else
+							{
+									
+									
+										echo  '<li class="nav-item"><a href="your_orders.php" class="nav-link active">your orders</a> </li>';
+									echo  '<li class="nav-item"><a href="logout.php" class="nav-link active">logout</a> </li>';
+							}
+
+						?>
+							 
+                        </ul>
+                  </div>
+               </div>
+            </nav>
+            <!-- /.navbar -->
+         </header>
+         <div class="page-wrapper">
+            <div class="breadcrumb">
+               <div class="container">
+                  <ul>
+                     <li><a href="#" class="active">
+					  <span style="color:red;"><?php echo $message; ?></span>
+					   <span style="color:green;">
+								<?php echo $success; ?>
+										</span>
+					   
+					</a></li>
+                    
+                  </ul>
+               </div>
+            </div>
+
+
+            </section>
+
+            <!-- start: FOOTER -->
+            <footer class="footer">
+                <div class="container">
+                    <!-- top footer statrs -->
+                    <div class="row top-footer">
+                        <div class="col-xs-12 col-sm-3 footer-logo-block color-gray">
+                            <a href="#"> <img src="images/logo.png" alt="Footer logo" height="60"> </a> <span>Order Delivery &amp; Take-Out </span> </div>
+                            
+
+                    <!-- top footer ends -->
+
+                                <div class="col-xs-12 col-sm-4 address color-gray">
+                                    <h5>Address</h5>
+                                    <p>Concept design of oline food order and deliveye,planned as restaurant directory</p>
+                                    <h5>Phone: <a href="tel:+080000012222">080 000012 222</a></h5> </div>
+                                <div class="col-xs-12 col-sm-5 additional-info color-gray">
+                                    <h5>Addition informations</h5>
+                                    <p>Join the thousands of other restaurants who benefit from having their menus on TakeOff</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- bottom footer ends -->
+                </div>
+            </footer>
+            <!-- end:Footer -->
+        </div>
+            <!-- end:Footer -->
+         </div>
+         <!-- end:page wrapper -->
+      
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/tether.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/animsition.min.js"></script>
+    <script src="js/bootstrap-slider.min.js"></script>
+    <script src="js/jquery.isotope.min.js"></script>
+    <script src="js/headroom.js"></script>
+    <script src="js/foodpicky.min.js"></script>
+</body>
+
+</html>
